3f2ac9986bc33691d549f484619eca7c5ca30c3a
Admin <admin@artesaniagualeguay.com>
2026-08-01 20:42:47 -0300

    debug: remove temporary diagnostic endpoint

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { login, debugAuth } = require('../controllers/authController');
+const { login } = require('../controllers/authController');
 
 router.post('/login', login);
-router.post('/debug-auth', debugAuth);
 
 module.exports = router;
