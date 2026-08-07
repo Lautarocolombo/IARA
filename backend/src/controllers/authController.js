61e77912809e9b368b66a032e48e191e4f9615bb
Admin <admin@artesaniagualeguay.com>
2026-08-07 14:13:20 -0300

    fix: replace bcrypt with bcryptjs to avoid native build issues in Render

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { query } = require('../lib/db');
 const logger = require('../lib/logger');
 
